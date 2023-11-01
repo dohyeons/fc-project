0c436dc71c753a9f4d62c63683a1a6b292f973f8
dohyeons <ddhhss0603@gmail.com>
2023-11-01 19:40:37 +0900

    design: 닉네임 입력 폼 렌더링

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-	return <div>홈 화면</div>;
+	return (
+		<form
+			onSubmit={e => {
+				e.preventDefault();
+			}}
+		>
+			<label htmlFor="nickName">닉네임을 입력하세요</label>
+			<input id="nickName" className="outline outline-black outline-1 p-1" />
+		</form>
+	);
 }
