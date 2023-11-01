7124cfccca1b4c5fc470e683e9c88780f8fb8027
dohyeons <ddhhss0603@gmail.com>
2023-11-01 21:13:31 +0900

    feat: 메인 페이지에서 match로 닉네임 쿼리 전달 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,33 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 export default function Home() {
+	const [nickName, setNickname] = useState("");
+	const router = useRouter();
+
+	function handleSubmit() {
+		router.push(`/match?nickName=${nickName}`);
+	}
+
+	function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+		setNickname(e.target.value);
+	}
 	return (
 		<form
 			onSubmit={e => {
 				e.preventDefault();
+				handleSubmit();
+				// router.push("/match");
 			}}
 		>
 			<label htmlFor="nickName">닉네임을 입력하세요</label>
-			<input id="nickName" className="outline outline-black outline-1 p-1" />
+			<input
+				id="nickName"
+				onChange={e => {
+					handleInput(e);
+				}}
+				className="outline outline-black outline-1 p-1"
+			/>
 		</form>
 	);
 }
