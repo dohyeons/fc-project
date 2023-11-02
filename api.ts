8b31abf6b3483eaf4cdf8c6bd5e79d31fcbd4332
dohyeons <ddhhss0603@gmail.com>
2023-11-02 16:52:27 +0900

    fix: 불필요한 헤더 설정 삭제

--- api.ts
+++ api.ts
@@ -1,18 +1,11 @@
 export async function fetchUserInformation(nickName: string) {
-	const requestHeaders: HeadersInit = new Headers();
-	requestHeaders.set("Content-Type", "application/json");
-	requestHeaders.set(
-		"Authorization",
-		`${process.env.NEXT_PUBLIC_API_MAIN_KEY}`
-	);
-
 	try {
 		const { accessId } = await fetch(
 			`https://public.api.nexon.com/openapi/fconline/v1.0/users?nickname=${nickName}`,
-			{ headers: requestHeaders }
+			{ headers: { Authorization: `${process.env.NEXT_PUBLIC_API_MAIN_KEY}` } }
 		).then(res => res.json());
-		return accessId || null;
-	} catch (error) {
+		return accessId;
+	} catch {
 		return null;
 	}
 }
