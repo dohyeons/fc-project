73f69ee05e7d0e7c7bf7ce1cd605068773b52193
dohyeons <ddhhss0603@gmail.com>
2023-11-03 17:42:59 +0900

    feat: 유저 최고 등급 조회

--- api.ts
+++ api.ts
@@ -9,3 +9,27 @@ export async function fetchUserInformation(nickName: string) {
 		return null;
 	}
 }
+
+export async function fetchUserMaxDivision(accessId: string) {
+	try {
+		const divisionData = await fetch(
+			`https://public.api.nexon.com/openapi/fconline/v1.0/users/${accessId}/maxdivision`,
+			{ headers: { Authorization: `${process.env.NEXT_PUBLIC_API_MAIN_KEY}` } }
+		).then(res => res.json());
+		return divisionData.map(
+			(obj: {
+				matchType: number;
+				division: number;
+				achievementDate: string;
+			}) => {
+				const date = new Date(obj.achievementDate);
+				const year = date.getFullYear();
+				const month = date.getMonth() + 1;
+				const day = date.getDate();
+				return { ...obj, achievementDate: `${year}년 ${month}월 ${day}일` };
+			}
+		);
+	} catch {
+		return null;
+	}
+}
