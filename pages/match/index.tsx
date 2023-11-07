2ba00115edec0cdde5864ebf9f43e9b6e31fc715
dohyeons <ddhhss0603@gmail.com>
2023-11-07 20:35:00 +0900

    feat: matchData 배열 요소 유무에 따른 조건부 렌더링

--- pages/match/index.tsx
+++ pages/match/index.tsx
@@ -39,9 +39,13 @@ export default function Match({
 					)
 				)}
 				<b>공식 경기 id</b>
-				{matchData?.map((matchId: string) => (
-					<div key={matchId}>{matchId}</div>
-				))}
+				{matchData.length ? (
+					matchData?.map((matchId: string) => (
+						<div key={matchId}>{matchId}</div>
+					))
+				) : (
+					<div>최근 공식 경기 정보가 존재하지 않습니다!</div>
+				)}
 			</div>
 		</>
 	);
