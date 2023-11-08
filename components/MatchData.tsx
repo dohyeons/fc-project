643fd35d35e0a7ed51b8064b49a3843991257711
dohyeons <ddhhss0603@gmail.com>
2023-11-08 19:14:18 +0900

    feat: 매치 정보 렌더링 구현 #5

--- components/MatchData.tsx
+++ components/MatchData.tsx
@@ -1,8 +1,17 @@
 interface MatchDataProps {
 	matchData: string[];
+	matchDetail:
+		| {
+				matchDate: string;
+				nickname1: string;
+				matchResult1: string;
+				nickname2: string;
+				matchResult2: string;
+		  }[]
+		| null;
 }
 
-export default function MatchData({ matchData }: MatchDataProps) {
+export default function MatchData({ matchData, matchDetail }: MatchDataProps) {
 	return (
 		<>
 			{matchData.length ? (
@@ -10,6 +19,11 @@ export default function MatchData({ matchData }: MatchDataProps) {
 			) : (
 				<div>최근 공식 경기 정보가 존재하지 않습니다!</div>
 			)}
+			{matchDetail?.map(el => (
+				<div key={el.matchDate}>
+					{`${el.nickname1}   ${el.matchResult1} vs ${el.nickname2}   ${el.matchResult2}`}
+				</div>
+			))}
 		</>
 	);
 }
