311bd9143ccbe167221369d1d509c9fd58037b77
dohyeons <ddhhss0603@gmail.com>
2023-11-08 19:14:04 +0900

    feat: Promise.all을 사용한 매치 정보 조회 구현 #5

--- api.ts
+++ api.ts
@@ -46,3 +46,96 @@ export async function fetchUserMatchData(accessId: string) {
 		return null;
 	}
 }
+
+export async function fetchMatchDetails(matchIds: string[]) {
+	const requests = matchIds.map(async id => {
+		return await fetch(
+			`https://public.api.nexon.com/openapi/fconline/v1.0/matches/${id}`,
+			{
+				headers: {
+					Authorization: `${process.env.NEXT_PUBLIC_API_MAIN_KEY}`,
+				},
+			}
+		);
+	});
+	const matchDetails: {
+		matchDate: string;
+		nickname1: string;
+		matchResult1: string;
+		nickname2: string;
+		matchResult2: string;
+	}[] = [];
+	await Promise.all(requests)
+		.then(responses => Promise.all(responses.map(r => r.json())))
+		.then(response => {
+			response.forEach(res => {
+				const {
+					matchDate,
+					matchInfo: [
+						{
+							nickname: nickname1,
+							matchDetail: { matchResult: matchResult1 },
+						},
+						{
+							nickname: nickname2,
+							matchDetail: { matchResult: matchResult2 },
+						},
+					],
+				} = res;
+				matchDetails.push({
+					matchDate,
+					nickname1,
+					matchResult1,
+					nickname2,
+					matchResult2,
+				});
+			});
+		});
+	return matchDetails;
+}
+
+// 챗 gpt가 작성해준 코드
+// export async function fetchMatchDetails(matchIds: string[]) {
+// 	try {
+// 		const results = await Promise.all(
+// 			matchIds.map(async matchId => {
+// 				try {
+// 					const response = await fetch(
+// 						`https://public.api.nexon.com/openapi/fconline/v1.0/matches/${matchId}`,
+// 						{
+// 							headers: {
+// 								Authorization: `${process.env.NEXT_PUBLIC_API_MAIN_KEY}`,
+// 							},
+// 						}
+// 					);
+// 					const {
+// 						matchDate,
+// 						matchInfo: [
+// 							{
+// 								nickname: nickname1,
+// 								matchDetail: { matchResult: matchResult1 },
+// 							},
+// 							{
+// 								nickname: nickname2,
+// 								matchDetail: { matchResult: matchResult2 },
+// 							},
+// 						],
+// 					} = await response.json();
+// 					return {
+// 						matchDate,
+// 						nickname1,
+// 						matchResult1,
+// 						nickname2,
+// 						matchResult2,
+// 					};
+// 				} catch {
+// 					return null;
+// 				}
+// 			})
+// 		);
+
+// 		return results;
+// 	} catch {
+// 		return null;
+// 	}
+// }
