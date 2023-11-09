7492cbe485b3bd1bdace8e83b97f83b08dfa2c8e
dohyeons <ddhhss0603@gmail.com>
2023-11-09 16:42:42 +0900

    design: 좌우 여백 20px 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,20 +13,21 @@ export default function Home() {
 		setNickname(e.target.value);
 	}
 	return (
-		<div className="flex flex-col items-center justify-center h-screen w-screen gap-4">
+		<div className="flex flex-col items-center justify-center h-screen w-full gap-4 ">
 			<h1 className="text-2xl font-semibold">너 X 못하잖아~</h1>
 			<form
 				onSubmit={e => {
 					e.preventDefault();
 					handleSubmit();
 				}}
+				className="w-full flex justify-center"
 			>
 				<input
 					id="nickName"
 					onChange={e => {
 						handleInput(e);
 					}}
-					className="outline outline-black outline-1"
+					className="outline outline-black outline-1 w-full max-w-xs"
 					required
 					placeholder="닉네임을 입력하세요"
 				/>
