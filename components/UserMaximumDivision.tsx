fcff39427044a6eb230f4248ef7de24806a30da1
dohyeons <ddhhss0603@gmail.com>
2023-11-11 22:17:02 +0900

    design: 티어 정보 컨테이너 안쪽 여백 설정

--- components/UserMaximumDivision.tsx
+++ components/UserMaximumDivision.tsx
@@ -12,7 +12,7 @@ export default function UserMaximumDivision({
 	userMaxDivisionData,
 }: UserMaximumDivisionProps) {
 	return (
-		<div className="border border-black 1px solid flex gap-3">
+		<div className="border border-black 1px solid flex gap-3 animate-fadeIn">
 			{userMaxDivisionData?.map(
 				(obj: {
 					matchType: number;
@@ -21,7 +21,7 @@ export default function UserMaximumDivision({
 				}) => (
 					<div
 						key={obj.matchType}
-						className="border 1px border-red-400 solid flex flex-col items-center gap-2"
+						className="border 1px border-red-400 solid flex flex-col items-center gap-2 px-2 py-3"
 					>
 						<h4 className="text-lg font-bold">{matchType[obj.matchType]}</h4>
 						<div className={`${division[obj.division][1]}`}>
