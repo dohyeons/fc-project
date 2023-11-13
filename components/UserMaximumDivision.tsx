5711c2c48a3cf0fa61836b541e524997cf418f94
dohyeons <ddhhss0603@gmail.com>
2023-11-13 15:33:39 +0900

    feat: 등급 정보가 존재하지 않을 경우 조건부 렌더링

--- components/UserMaximumDivision.tsx
+++ components/UserMaximumDivision.tsx
@@ -13,27 +13,31 @@ export default function UserMaximumDivision({
 }: UserMaximumDivisionProps) {
 	return (
 		<div className="border border-black 1px solid flex gap-3 animate-fadeIn">
-			{userMaxDivisionData?.map(
-				(obj: {
-					matchType: number;
-					division: number;
-					achievementDate: string;
-				}) => (
-					<div
-						key={obj.matchType}
-						className="border 1px border-red-400 solid flex flex-col items-center gap-2 px-2 py-3 shadow-md"
-					>
-						<h4 className="text-lg font-bold">{matchType[obj.matchType]}</h4>
+			{userMaxDivisionData.length ? (
+				userMaxDivisionData.map(
+					(obj: {
+						matchType: number;
+						division: number;
+						achievementDate: string;
+					}) => (
 						<div
-							className={`${
-								division[obj.division][1]
-							} w-[100px] h-24 rounded-xl flex justify-center items-center font-semibold`}
+							key={obj.matchType}
+							className="border 1px border-red-400 solid flex flex-col items-center gap-2 px-2 py-3 shadow-md"
 						>
-							{division[obj.division][0]}
+							<h4 className="text-lg font-bold">{matchType[obj.matchType]}</h4>
+							<div
+								className={`${
+									division[obj.division][1]
+								} w-[100px] h-24 rounded-xl flex justify-center items-center font-semibold`}
+							>
+								{division[obj.division][0]}
+							</div>
+							<div>{obj.achievementDate}</div>
 						</div>
-						<div>{obj.achievementDate}</div>
-					</div>
+					)
 				)
+			) : (
+				<div>등급 정보가 존재하지 않습니다!</div>
 			)}
 		</div>
 	);
