daec4f4781d0ec7f0459c08292c4236432908ffa
dohyeons <ddhhss0603@gmail.com>
2023-11-20 20:29:53 +0900

    feat: SubLayout 조건부 렌더링 #8

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import { ReactNode } from "react";
+interface PageWithLayout {
+	Layout?: React.ComponentType;
+}
 export default function App({ Component, pageProps }: AppProps) {
+	const EmptyLayout = ({ children }: { children: ReactNode }) => (
+		<>{children}</>
+	);
+	const SubLayout = (Component as PageWithLayout).Layout || EmptyLayout;
+
 	return (
 		<Layout>
-			<Component {...pageProps} />
+			<SubLayout>
+				<Component {...pageProps} />
+			</SubLayout>
 		</Layout>
 	);
 }
