f122ef098b78a4b087a376ac4d0caf1a0f73e7c9
dohyeons <ddhhss0603@gmail.com>
2023-11-28 20:18:48 +0900

    design: 매치 일자와 시간 분리 #8

--- components/MatchData.tsx
+++ components/MatchData.tsx
@@ -13,7 +13,7 @@ export default function MatchData({ matchDetail }: MatchDataProps) {
 		<>
 			{matchDetail.map(el => (
 				<div key={el.matchDate[1] + el.matchDate[0]}>
-					<p>{el.matchDate}</p>
+					<p>{`${el.matchDate[0]} ${el.matchDate[1]}`}</p>
 					{`${el.nickname1}   ${el.matchResult1} vs ${el.nickname2}   ${el.matchResult2}`}
 				</div>
 			))}
