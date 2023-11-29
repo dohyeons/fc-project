e3e6ca775d30bd159d8390ecdb0390930330db3a
dohyeons <ddhhss0603@gmail.com>
2023-11-29 22:45:27 +0900

    feat: 매치 넘버링 #8

--- components/MatchData.tsx
+++ components/MatchData.tsx
@@ -14,8 +14,9 @@ export default function MatchData({ matchDetail }: MatchDataProps) {
 			{matchDetail.map((el, idx) => (
 				<div
 					key={el.matchDate[1] + el.matchDate[0]}
-					className="border border-violet-600 p-5 w-full flex flex-col items-center"
+					className="border border-violet-600 p-5 w-full flex items-center"
 				>
+					<div>{idx + 1}</div>
 					<div className="border border-green-600 flex flex-col items-center">
 						<p>{`${el.matchDate[0]} ${el.matchDate[1]}`}</p>
 						<p>{`${el.nickname1}   ${el.matchResult1} vs ${el.nickname2}   ${el.matchResult2}`}</p>
