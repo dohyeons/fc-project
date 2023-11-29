b4e2d6aed29989604f98ecd32b1155446d66d4be
dohyeons <ddhhss0603@gmail.com>
2023-11-29 22:44:08 +0900

    design: 매치 정보 임시 테두리 #7

--- components/MatchData.tsx
+++ components/MatchData.tsx
@@ -11,12 +11,12 @@ interface MatchDataProps {
 export default function MatchData({ matchDetail }: MatchDataProps) {
 	return (
 		<>
-			{matchDetail.map(el => (
+			{matchDetail.map((el, idx) => (
 				<div
 					key={el.matchDate[1] + el.matchDate[0]}
 					className="border border-violet-600 p-5 w-full flex flex-col items-center"
 				>
-					<div className="flex flex-col items-center">
+					<div className="border border-green-600 flex flex-col items-center">
 						<p>{`${el.matchDate[0]} ${el.matchDate[1]}`}</p>
 						<p>{`${el.nickname1}   ${el.matchResult1} vs ${el.nickname2}   ${el.matchResult2}`}</p>
 					</div>
